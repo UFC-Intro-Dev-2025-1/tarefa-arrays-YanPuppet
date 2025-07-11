43cecf6e708dbd951499c5fbd2101fed7ed6df85
Maria Clara Sousa de Melo <205983934+YanPuppet@users.noreply.github.com>
2025-07-11 17:42:33 +0000

    solução

--- src/main.ts
+++ src/main.ts
@@ -18,26 +18,31 @@
 const numeros: number[] = [10, 20, 30, 40, 50];
 
 function mostrarArray(array: number[]): void {
-    console.log('--------------------------------');
-    console.log(`Tamanho: `);
-    console.log(`Array: `);
-}
-mostrarArray(numeros);
+    console.log(array);
+    console.log(array.length);
+    }
+    numeros.push(60,70)
+    mostrarArray(numeros);
 
 
 
 
 
-const elementoExcluido = ;
+const elementoExcluido = 70;
+var ultimo = numeros.pop();
 mostrarArray(numeros);
 console.log(`Elemento excluído: ${elementoExcluido}`);
 
-numeros.forEach();
+numeros.forEach(function(item, indice, array){
+    console.log(item, indice);
+});
 
-const dobrados = numeros.map();
+const dobrados = numeros.map((x) => x * 2);
 mostrarArray(dobrados);
 
-const soma = numeros.reduce();
+const initialValue = 0;
+const soma = numeros.reduce( 
+    (accumulator, currentValue) => accumulator + currentValue, initialValue);
 console.log(`Soma dos elementos do array: ${soma}`);
 
 // para rodar o código, use o comando:
